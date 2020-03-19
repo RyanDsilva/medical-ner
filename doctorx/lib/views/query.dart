571e311a26bef146986c978efc3a5be7bf2394a6
Ryan Dsilva <ryan.dsilva.98@gmail.com>
2020-03-19 21:52:42 +0530

    completed ocr and query page

--- doctorx/lib/views/query.dart
+++ doctorx/lib/views/query.dart
@@ -1,6 +1,12 @@
+import 'dart:io';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'package:flushbar/flushbar.dart';
+import 'package:flushbar/flushbar_helper.dart';
+import 'package:flushbar/flushbar_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:getflutter/getflutter.dart';
+import 'package:image_picker/image_picker.dart';
 
 class QueryPage extends StatefulWidget {
   QueryPage({Key key}) : super(key: key);
@@ -10,6 +16,47 @@ class QueryPage extends StatefulWidget {
 }
 
 class _QueryPageState extends State<QueryPage> {
+  TextEditingController _queryController = new TextEditingController();
+  String _text = '';
+  File _image;
+
+  void getImage(int source) async {
+    if (source == 1) {
+      var imageSrc = await ImagePicker.pickImage(source: ImageSource.camera);
+      setState(() {
+        _text = '';
+        _image = imageSrc;
+      });
+      performOCR();
+    } else if (source == 2) {
+      var imageSrc = await ImagePicker.pickImage(source: ImageSource.gallery);
+      setState(() {
+        _text = '';
+        _image = imageSrc;
+      });
+      performOCR();
+    } else {
+      showFlushbar(
+        context: context,
+        flushbar: FlushbarHelper.createError(
+          message: 'Invalid Image Source',
+        )..show(context),
+      );
+    }
+  }
+
+  void performOCR() async {
+    final FirebaseVisionImage _ocrImage =
+        FirebaseVisionImage.fromFile(this._image);
+    final TextRecognizer _textRecognizer =
+        FirebaseVision.instance.textRecognizer();
+    final VisionText visionText = await _textRecognizer.processImage(_ocrImage);
+    setState(() {
+      this._text = visionText.text.replaceAll("\n", " ");
+    });
+    _textRecognizer.close();
+  }
+
   @override
   Widget build(BuildContext context) {
     ScreenUtil.init(context, width: 720, height: 1280, allowFontScaling: true);
@@ -36,10 +83,88 @@ class _QueryPageState extends State<QueryPage> {
         centerTitle: true,
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Text('Home'),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Text(
+                    'Query',
+                    style: TextStyle(
+                      color: Theme.of(context).accentColor,
+                      fontWeight: FontWeight.bold,
+                      fontSize: ScreenUtil().setSp(45),
+                    ),
+                  ),
+                  TextField(
+                    controller: _queryController,
+                    maxLines: 5,
+                    decoration: InputDecoration(
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Theme.of(context).accentColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: ScreenUtil().setHeight(25)),
+                  Text(
+                    'OR',
+                    style: TextStyle(
+                      color: Theme.of(context).accentColor,
+                      fontWeight: FontWeight.bold,
+                      fontSize: ScreenUtil().setSp(45),
+                    ),
+                  ),
+                  SizedBox(height: ScreenUtil().setHeight(15)),
+                  GFButton(
+                    buttonBoxShadow: true,
+                    enableFeedback: true,
+                    size: GFSize.LARGE,
+                    onPressed: () => this.getImage(2),
+                    text: "Scan Document",
+                    textStyle: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: ScreenUtil().setSp(40),
+                      fontWeight: FontWeight.bold,
+                    ),
+                    icon: Icon(
+                      Icons.photo_camera,
+                      color: Colors.white,
+                      size: ScreenUtil().setWidth(50),
+                    ),
+                    type: GFButtonType.solid,
+                    fullWidthButton: true,
+                    color: Theme.of(context).accentColor,
+                  ),
+                  SizedBox(height: ScreenUtil().setHeight(30)),
+                  Text(this._text),
+                  SizedBox(height: ScreenUtil().setHeight(30)),
+                  GFButton(
+                    buttonBoxShadow: true,
+                    enableFeedback: true,
+                    size: GFSize.LARGE,
+                    onPressed: () => {},
+                    text: "Submit Query",
+                    textStyle: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: ScreenUtil().setSp(40),
+                      fontWeight: FontWeight.bold,
+                    ),
+                    icon: Icon(
+                      Icons.send,
+                      color: Colors.white,
+                      size: ScreenUtil().setWidth(50),
+                    ),
+                    type: GFButtonType.solid,
+                    fullWidthButton: true,
+                    color: Theme.of(context).accentColor,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
